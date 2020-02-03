1b1adf3bc2b7b225d1ebf3295745425d848ed36f
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2020-02-03 14:01:18 +0100

    aa

--- Canvas/2DTransforms.py
+++ Canvas/2DTransforms.py
@@ -24,51 +24,55 @@ def getrotation(deg:float)->list:
     rad = np.radians(deg)
     return [[np.cos(rad), np.sin(rad), 0], [-np.sin(rad), np.cos(rad), 0], [0, 0, 1]]
 
-# def crearLineas():
-
-triangle = [[25, 50], [60, 75], [60, 25]]
 
 root = Tk()
 f = ttk.Frame()
 f.pack()
-cnv = Canvas(f, width=400, height=400)
+cnv = Canvas(f, width=800, height=800)
 cnv.grid(row=0, column=0)
-cnv.create_polygon(triangle, outline='#f11', fill='#1f1', width=2)
-
-homogenize(triangle)
-triangle2 = list()
-toorig = gettranslation(-48, -50)
-topos = gettranslation(120, 50)
-rotation = getrotation(180)
-transform = np.dot(toorig, rotation)
-transform = np.dot(transform, topos)
-for i in range(len(triangle)):
-    triangle2.append(np.dot(triangle[i],transform).tolist())
-dehomogenize(triangle2)
-
-cnv.create_polygon(triangle2, outline='#f11', fill='#1f1', width=2)
 
 # Círculo central
-ovalo = [180,180,210,210]
+ovalo = [360,360,420,420]
 cnv.create_oval(ovalo, fill='#f11', width=4)
 
 # Líneas velocímetro
-linea = [[15, 195], [45, 195]]
-cnv.create_line(linea, width=3)
-
+linea = [[-350, 0],[-300, 0]]
 homogenize(linea)
-toorig = gettranslation(-195, -165)
+# Le pasamos los puntos de la línea respecto al origen de coordenadas respectivamente corresponden a 'x' e 'y'
+toorig = gettranslation(400, 400)
+for rot in range(-20,220,20):
+    rotation = getrotation(rot)
+    transform = np.dot(rotation, toorig)
+    linea2 = list()
+    for z in range(len(linea)):
+        linea2.append(np.dot(linea[z], transform).tolist())
+    dehomogenize(linea2)
+    print(linea2)
+    cnv.create_line(linea2, width=2)
 
-for rot in range(0, 220, 20):
+large = [[-350, 0],[-280, 0]]
+homogenize(large)
+toorig = gettranslation(400, 400)
+for rot in range(-30,220,20):
     rotation = getrotation(rot)
     transform = np.dot(rotation, toorig)
     temp = list()
     for i in range(len(linea)):
-        temp.append((np.dot(linea[rot], transform).tolist()))
-    dehomogenize(linea)
-    cnv.create_line(temp, width=2)
+        temp.append(np.dot(large[i],transform).tolist())
+    dehomogenize(temp)
+    cnv.create_line(temp, width=3)
 
+needle = [[0,0], [-7, -7], [0, -150], [7, -7]]
+homogenize(needle)
+toorig = gettranslation(400, 400)
+needle2 = list()
+for i in range(len(needle)):
+    needle2.append(np.dot(needle[i],toorig).tolist())
+dehomogenize(needle2)
+cnv.create_polygon(needle2, outline='#f11', fill='#1f1', width=3)
 
+# marca = [[350, 0],[290, 0]]
+# cnv.create_line(marca, width=3)
 
 
 root.mainloop()
